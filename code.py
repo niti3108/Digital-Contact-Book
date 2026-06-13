28e4262cbd8ef0bf267dec9057840ab5e3e167e5
niti3108 <nitig5388@gmail.com>
2026-06-13 12:38:26 +0530

    code.py
    Implemented a digital contact book that allows adding and searching for contacts.

--- code.py
+++ code.py
@@ -1,1 +1,19 @@
+# Digital Contact Book
 
+contacts = {}
+
+# Add 3 contacts
+for i in range(3):
+    print(f"\nEnter Contact {i + 1}")
+    name = input("Name: ")
+    phone = input("Phone Number: ")
+    contacts[name] = phone
+
+# Search for a contact
+print("\n--- Search Contact ---")
+search_name = input("Enter the name to search: ")
+
+if search_name in contacts:
+    print("Phone Number:", contacts[search_name])
+else:
+    print("Contact not found!")
